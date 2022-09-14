c9986dda84fce0039a213c3ffd9a1b5a43db37b5
Thamires Pieve <thamires.pieve@gec.inatel.br>
2022-09-13 21:08:24 -0300

    fix: tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -12,8 +12,8 @@ function App() {
       <div className="max-w-[1344px] mx-auto flex flex-col items-center  my-20">
         <img src={logo} alt="Logo nlw eSports" />
 
-        <h1 className="text-6xl text-white font-black mt-20">Seu
-         <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui.
+        <h1 className="text-6xl text-white font-black mt-20">Seu 
+         <span className="text-transparent bg-nlw-gradient bg-clip-text"> duo</span> está aqui.
          </h1>
 
         <div className="grid grid-cols-6 gap-6 mt-16">
